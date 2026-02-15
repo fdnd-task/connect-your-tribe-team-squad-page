9bbfdb1484b6fa0cfd2ef84892aac7f53edf5cdf
Krijn Hoetmer <krijnhoetmer@gmail.com>
2026-02-15 22:57:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,31 @@ app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', async function (request, response) {
-  const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
+
+  // Filter eerst de berichten die je wilt zien, net als bij personen
+  // Deze tabel wordt gedeeld door iedereen, dus verzin zelf een handig filter,
+  // bijvoorbeeld je teamnaam, je projectnaam, je person ID, de datum van vandaag, etc..
+  const params = {
+    'filter[for]': `Team ${teamName}`,
+  }
+
+  // Maak hiermee de URL aan, zoals we dat ook in de browser deden
+  const apiURL = 'https://fdnd.directus.app/items/messages?' + new URLSearchParams(params)
+
+  // Laat eventueel zien wat de filter URL is
+  // (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
+  // console.log('API URL voor messages:', apiURL)
+
+  // Haal daarna de messages data op
+  const messagesResponse = await fetch(apiURL)
+
+  // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
   const messagesResponseJSON = await messagesResponse.json()
 
+  // Controleer eventueel de data in je console
+  // console.log(messagesResponseJSON)
+
+  // En render de view met de messages
   response.render('index.liquid', {
     teamName: teamName,
     messages: messagesResponseJSON.data
@@ -30,18 +52,31 @@ app.get('/', async function (request, response) {
 })
 
 app.post('/', async function (request, response) {
-  await fetch('https://fdnd.directus.app/items/messages/', {
+
+  // Stuur een POST request naar de messages tabel
+  // Een POST request bevat ook extra parameters, naast een URL
+  await fetch('https://fdnd.directus.app/items/messages', {
+
+    // Overschrijf de standaard GET method, want ook hier gaan we iets veranderen op de server
     method: 'POST',
+
+    // Geef de body mee als JSON string
     body: JSON.stringify({
+      // Dit is zodat we ons bericht straks weer terug kunnen vinden met ons filter
       for: `Team ${teamName}`,
+      // En dit zijn onze formuliervelden
       from: request.body.from,
       text: request.body.text
     }),
+
+    // En vergeet deze HTTP headers niet: hiermee vertellen we de server dat we JSON doorsturen
+    // (In realistischere projecten zou je hier ook authentication headers of een sleutel meegeven)
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
     }
   });
 
+  // Stuur de browser daarna weer naar de homepage
   response.redirect(303, '/')
 })
 
